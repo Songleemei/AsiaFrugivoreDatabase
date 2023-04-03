3a26b35ccced8f17ce143d9920ddbc4e706a083a
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-03 17:15:34 +0800

    Update test-periods-ga.R
    Put the resulted highlighted data into new forlder (data-output) to store all the resulted error reports

--- testthat/test-periods-ga.R
+++ testthat/test-periods-ga.R
@@ -1,15 +1,20 @@
 library(testthat)
 library(dplyr)
 library(readr)
+library(colorDF)
 
 context("Checks that all values in period variable are valid.")
 
-base_data <- read_csv("../data-raw/data.csv")
+base_data <- read_csv("../data-raw/data1.csv")
 
 test_that(
   desc = "Period values are valid.",
   code = {
     all_period_values_valid <- all(base_data$period < 1000)
-    expect_true(all_period_values_valid)
+    # expect_true(all_period_values_valid)
+    if (all_period_values_valid == FALSE){
+      highlight(base_data, base_data$period > 1000)
+      write_csv(base_data, "../data-output/data_report.csv")
+    }
   }
 )
