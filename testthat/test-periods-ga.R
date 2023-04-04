5bfbbdb95432b218d11bf5f33c7e848143cfd23a
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-04 17:09:59 +0800

    Store the result to the data-output folder in the remote #2

--- testthat/test-periods-ga.R
+++ testthat/test-periods-ga.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(dplyr)
+#library(utils)
 library(readr)
 library(git2rdata)
 #library(colorDF)
@@ -15,8 +16,9 @@ test_that(
     # expect_true(all_period_values_valid)
     if (all_period_values_valid == FALSE){
       flag_error <- subset(base_data, period > 1000)
-      write_vc(flag_error, file = "/data-output/data_flag_error.csv", root = ".")
+    # write_vc(flag_error, file = "/data-output/data_flag_error.csv", root = ".")
     # write_csv(flag_error, "../data-output/data_flag_error.csv")
+      write.csv(flag_error, file.path(here("data-output"), "data_flag_error.csv"), row.names = F)
     # For non-error one (warning), can name the file as "data_flag_warning.csv")
     }
   }
