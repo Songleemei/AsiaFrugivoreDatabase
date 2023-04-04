310e382425de173468d621867122b8bb09a90630
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-04 19:47:49 +0800

    Created a new standard for error and write the result in the data-output folder #2

--- testthat/test-periods-ga.R
+++ testthat/test-periods-ga.R
@@ -9,17 +9,20 @@ context("Checks that all values in period variable are valid.")
 
 base_data <- read_csv("../data-raw/data.csv")
 
+all_period_values_valid <- all(base_data$period < 465)
+
+if (all_period_values_valid == FALSE){
+  flag_error <- subset(base_data, period > 465)
+  # write_vc(flag_error, file = "/data-output/data_flag_error.csv", root = ".")
+  # write_csv(flag_error, "../data-output/data_flag_error.csv")
+  write.csv(flag_error, "../data-output/data_flag_error.csv", row.names = F)
+  # For non-error one (warning), can name the file as "data_flag_warning.csv")
+}
+
 test_that(
   desc = "Period values are valid.",
   code = {
-    all_period_values_valid <- all(base_data$period < 465)
     expect_true(all_period_values_valid)
-    if (all_period_values_valid == FALSE){
-      flag_error <- subset(base_data, period > 465)
-    # write_vc(flag_error, file = "/data-output/data_flag_error.csv", root = ".")
-    # write_csv(flag_error, "../data-output/data_flag_error.csv")
-      write.csv(flag_error, "../data-output/data_flag_error.csv", row.names = F)
-    # For non-error one (warning), can name the file as "data_flag_warning.csv")
-    }
   }
 )
+
