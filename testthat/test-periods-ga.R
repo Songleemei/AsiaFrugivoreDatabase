7ab407a69c7ecfd889999072b50546d1dc70b796
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-04 17:56:34 +0800

    Created a new standard for error and write the result in the data-output folder

--- testthat/test-periods-ga.R
+++ testthat/test-periods-ga.R
@@ -12,10 +12,10 @@ base_data <- read_csv("../data-raw/data.csv")
 test_that(
   desc = "Period values are valid.",
   code = {
-    all_period_values_valid <- all(base_data$period < 1000)
-    #expect_true(all_period_values_valid)
+    all_period_values_valid <- all(base_data$period < 465)
+    expect_true(all_period_values_valid)
     if (all_period_values_valid == FALSE){
-      flag_error <- subset(base_data, period > 1000)
+      flag_error <- subset(base_data, period > 465)
     # write_vc(flag_error, file = "/data-output/data_flag_error.csv", root = ".")
     # write_csv(flag_error, "../data-output/data_flag_error.csv")
       write.csv(flag_error, "../data-output/data_flag_error.csv", row.names = F)
