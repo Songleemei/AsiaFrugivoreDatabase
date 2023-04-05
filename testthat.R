01ff7414fb99e9198d7dbe235cbc24950a36df1a
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-05 15:35:30 +0800

    Create an initial successful template first

--- testthat.R
+++ testthat.R
@@ -1,4 +1,5 @@
 # The reporter options ensure that the build will fail if any of the tests fail
+# This file directs the testing towards the testthat/ folder, whereby all files of form test-***.R are run.
 
 library(testthat)
 
