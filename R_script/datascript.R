406725d4667a7bb5f7ce6a5bf48637779b9811ea
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-06 00:40:09 +0800

    Create report forlder to replace data-output #7

--- R_script/datascript.R
+++ R_script/datascript.R
@@ -4,14 +4,14 @@ library(dplyr)
 library(readr)
 library(git2rdata)
 
-setwd(".")
-base_data <- read_csv("./data-raw/data1.csv")
+
+base_data <- read_csv("../data-raw/data1.csv")
 
 all_period_values_valid <- all(base_data$period < 1000)
 
 if (all_period_values_valid == FALSE){
   flag_error <- subset(base_data, period > 1000)
-  write.csv(flag_error, file = file.path(here("testthat"), "reports/data_flag_error.csv"), row.names = F)
+  write.csv(flag_error, "testthat/reports/data_flag_error.csv", row.names = F)
 }
 
 
