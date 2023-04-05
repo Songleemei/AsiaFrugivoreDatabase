15011a1a246f488a7191a62deb04eea1af1fea04
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-05 17:39:28 +0800

    Create report folder to replace data-output #3

--- R_script/datascript.R
+++ R_script/datascript.R
@@ -1,15 +1,17 @@
 # print("Have this script run whatever data cleaning you do")
-
+library(here)
 library(dplyr)
 library(readr)
 library(git2rdata)
 
-base_data <- read_csv("./data-raw/data.csv")
+base_data <- read_csv("../data-raw/data1.csv")
 
 all_period_values_valid <- all(base_data$period < 1000)
 
 if (all_period_values_valid == FALSE){
   flag_error <- subset(base_data, period > 1000)
-  write.csv(flag_error, "./data-output/data_flag_error.csv", row.names = F)
+  write.csv(flag_error, file = file.path(here("testthat"), "reports/data_flag_error.csv"), row.names = F)
 }
+
+
 
--- testthat/test-datascript.R
+++ testthat/test-datascript.R
@@ -1,4 +1,4 @@
-#library(here)
+library(here)
 library(testthat)
 context("checks that datascript is working")
 
@@ -11,8 +11,7 @@ context("checks that datascript is working")
 
 test_that("There are flgged errors", {
 
-  report_exists  <-  ifelse(file.exists("./data-output/data_flag_error.csv"), TRUE, FALSE)
-  #report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/requires_field_fix/require_field_fix_error_file.csv")), TRUE, FALSE)
+  report_exists  <-  ifelse(file.exists(file.path(here("testthat"), "reports/data_flag_error.csv")), TRUE, FALSE)
 
   expect_false(report_exists)
 })
