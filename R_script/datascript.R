748289b0b7175292c47216f2035fc18538e6865b
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-07 16:42:55 +0800

    To test why data1.csv doesn't exit in work directory

--- R_script/datascript.R
+++ R_script/datascript.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(readr)
 library(git2rdata)
 
-base_data <- read_csv("../data-raw/data1.csv")
+base_data <- read_csv("https://github.com/Songleemei/AsiaFrugivoreDatabase/blob/main/data-raw/data1.csv")
 
 all_period_values_valid <- all(base_data$period < 1000)
 
