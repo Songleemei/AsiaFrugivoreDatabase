3de8216e72e69b551c629f3d1bfd8b012ab41706
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-07 21:45:00 +0800

    Redo

--- R_script/datascript.R
+++ R_script/datascript.R
@@ -1,13 +1,11 @@
 # print("Have this script run whatever data cleaning you do")
-#library(here)
+library(here)
 library(dplyr)
 library(readr)
-library(git2rdata)
+#library(git2rdata)
 
 # using a git repository
-repo <- repository(".")
-#pull(repo)
-#write_vc(my_data, file = "rel_path/filename", root = repo, stage = TRUE)
+#repo <- repository(".")
 
 base_data <- read_csv("data-raw/data.csv")
 
@@ -17,9 +15,9 @@ all_period_values_valid <- all(base_data$period < 1000)
 
 if (all_period_values_valid == FALSE){
   flag_error <- subset(base_data, period > 1000)
-  write_vc(flag_error, file = "testthat/reports/data_flag_error.csv", root = repo, stage = TRUE)
+  #write_vc(flag_error, file = "testthat/reports/data_flag_error.csv", root = repo, stage = TRUE)
   #write_vc(flag_error, file = file.path(here("testthat"),"reports/data_flag_error.csv"), root = repo, stage = TRUE)
-  #write_csv(flag_error, file.path(here("testthat"),"reports/data_flag_error.csv"))
+  write_csv(flag_error, file.path(here("testthat"),"reports/data_flag_error.csv"))
 }
 
 
