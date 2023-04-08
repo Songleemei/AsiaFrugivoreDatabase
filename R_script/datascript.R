1d5bd0c3d0689f31dcbfd0ba1b8a4208dc2fb3d5
Hanci Liang <92319822+Songleemei@users.noreply.github.com>
2023-04-08 13:04:28 +0800

    Test #16: how to store the new result in the reports folder

--- R_script/datascript.R
+++ R_script/datascript.R
@@ -14,13 +14,12 @@ base_data <- read.csv(raw_data_path)
 
 #all_period_values_valid <- all(base_data$period < 460)
 
-#if (all_period_values_valid == FALSE){
+if (all_period_values_valid == FALSE){
   flag_error <- subset(base_data, period > 460)
   #write_vc(flag_error, file = "testthat/reports/data_flag_error.csv", root = repo, stage = TRUE)
   #write_vc(flag_error, file = file.path(here("testthat"),"reports/data_flag_error.csv"), root = repo, stage = TRUE)
   #write_csv(flag_error, file.path(here("testthat"),"reports/data_flag_error.csv"))
   write.csv(flag_error, file = file.path(here("testthat"), "reports/data_flag_error.csv"), row.names = F)
   #write.csv(flag_error, file = paste0(dirname(getwd()), "/testthat/reports/data_flag_error.csv"), row.names = F)
-
-#}
+}
 
